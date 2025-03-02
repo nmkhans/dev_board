11708808c3288f260d000020edb58096e8b5ee88
nmkhans <nurmoin05@gmail.com>
2025-03-02 23:31:48 +0600

    blog page ready

--- scripts/navigate.js
+++ scripts/navigate.js
@@ -1,5 +1,12 @@
-const blogCta = document.getElementById("blog__cta")
+const blogCta = document.getElementById("blog__cta");
+const backBtn = document.getElementById("back__btn");
 
-blogCta.addEventListener("click", function() {
-  window.location.href = "blog.html"
+blogCta?.addEventListener("click", function() {
+  window.location.href = "/blog.html"
+})
+
+
+backBtn?.addEventListener("click", function() {
+  console.log("clicked")
+  window.location.href = "/index.html";
 })
