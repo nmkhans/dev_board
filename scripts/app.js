cb3d678b364cde42ab5dc16b022ba0c680d6ffd0
nmkhans <nurmoin05@gmail.com>
2025-03-02 22:13:26 +0600

    activity log creation feature added

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ const completedTaskCount = document.getElementById("completed__task");
 const assignedTaskCount = document.getElementById("assigned__task");
 const taskbtnList = document.getElementsByClassName("task__btn");
 const activityLogList = document.getElementById("activity__log");
-const clearHistoryBtn = document.getElementById("clear__history")
+const clearHistoryBtn = document.getElementById("clear__history");
 
 for (const taskBtn of taskbtnList) {
   taskBtn.addEventListener("click", completeTask);
@@ -25,11 +25,33 @@ function updateAssignedTask(element) {
   let currentAssignedTask = parseInt(element.innerText);
   let newAssignedTask = currentAssignedTask - 1;
 
-  if(newAssignedTask === 0) {
-    alert("Congratulations! You have completed all the task.")
+  if (newAssignedTask === 0) {
+    alert("Congratulations! You have completed all the task.");
   }
 
   element.innerText = `0${newAssignedTask}`;
+}
+
+function createActivityLog(element) {
+  const taskTitle =
+    element.parentNode.parentNode.parentNode.getElementsByTagName(
+      "h3"
+    )[0].innerText;
+
+  const date = new Date();
+  const completedTime = date.toLocaleTimeString();
+
+  const div = document.createElement("div");
+  div.classList = "bg-[#F4F7FF] p-4 rounded-xl";
+  div.innerHTML = `
+    <p>
+      You have Complete The Task
+      <span class="font-semibold">${taskTitle}</span> at
+      ${completedTime}
+    </p>
+  `;
+
+  activityLogList.appendChild(div);
 }
 
 function completeTask(e) {
@@ -40,4 +62,12 @@ function completeTask(e) {
   updateCompletedTask(completedTaskCount);
 
   updateAssignedTask(assignedTaskCount);
+
+  createActivityLog(e.target);
+}
+
+clearHistoryBtn.addEventListener("click", clearHistory);
+
+function clearHistory(e) {
+  activityLogList.innerHTML = "";
 }
